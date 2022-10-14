ce7067c1a08c8e1c326dea35772eff04879610e3
samarthagali <samarthprasad440@gmail.com>
2022-10-14 23:16:45 +0530

    initial version of prev

--- shell/shell.c
+++ shell/shell.c
@@ -1,7 +1,25 @@
 #include "mcat.h"
 #include "mgrep.h"
 #include "mrev.h"
-#include <ncurses.h>
+
+void gotomcat(char * line);
+
+void gotogrep(char * line);
+
+void gotorev(char * line);
+
+void showprev(int no);
+
+void execprev(int no);
+
+int checkprev(char * line);
+
+void exec(char * line);
+
+
+char commands[20][200];
+int pos=0;
+
 void gotomcat(char * line){
     char * writer="mcat e";
     char * delim=" ";
@@ -90,13 +108,96 @@ void gotorev(char * line){
     writeinto(second,last);
 }
 
+void showprev(int no){
+    int i;
+    i=(pos-no-1)%20;
+    printf("%s",commands[i]);
+}
+
+void execprevcom(int no){
+    int i;
+    i=(pos-no-1)%20;
+    exec(commands[i]);
+}
+int count;
+char nums[]={"0123456789"};
+int checkprev(char * line){
+    char * delim=" ";
+    char newl[200];
+    char *ex="exec",* show="show",*prev="prev";
+    memcpy(newl,line,strlen(line));
+    char * token =strtok(line,delim);
+    int k=strcmp(ex,token);
+    int j=strcmp(show,token);
+    if(k==0||j==0){
+        char last[200],second[200];
+        int i =0;   
+        while(token!=NULL){
+            i++;
+            if(i==2){
+                strcpy(second,token);
+            }
+            memcpy(last,token,strlen(token));
+            token=strtok(NULL,delim);
+        }
+        // printf("\n\n\n\n%s",last);
+        printf("%c",last[0]);
+        last[strlen(last)-1]='\0';
+        if(strcmp(prev,second)&&i<=2){
+            if(k){
+                if(strlen(second)==4){
+                    showprev(1);
+                    return 1;
+                }
+           
+                if(isdigit(second[4])){
+                    showprev((int)second[5]);
+                    return 1;
+                }
+                else{
+                    printf("invalid command pls enter a no after prev ");
+                    return 1;
+                    }
+            }
+            if(i==2){
+                showprev(1);
+                return 1;
+            }
+            if(strchr(nums,last[0])){
+                printf("in here");
+                showprev((int)last[0]);
+                return 1;}
+            printf("invalid command pls enter a no after prev");
+            return 1;
+        }
+        return 0;
+        
+    }
+    return 0;
+}
+
+
 void exec(char * line){
     char *q="quit";
     char * cat="mcat ";
     char * grep="mgrep ";
     char * rev="mrev ";
     char check[5];
+    strcpy(commands[pos],line);
+    pos=(pos+1)%20;
+    count++;
+    if (strstr(line," prev")&&count>1 ){
+        
+    int i=checkprev(line);
+    if(i==1)
+        return;}
     if (strstr(line,q)){
+       int i=0;
+    //    printf("commands entered are:\n");
+    //    for (i=0;i<pos;i++){
+    //         printf("%s",commands[i]);
+    //    }
+       printf("\n\n");
         printf("\nexiting sam shell\n");
         exit(0);
         }
@@ -122,6 +223,7 @@ void main(int argc,char** argv){
         printf("sam-shell>");
         fgets(line, sizeof(line), stdin);
         exec(line);
+        //
     }
     }
     else{
@@ -129,3 +231,5 @@ void main(int argc,char** argv){
         exit(1);
     }
 }
+
+
